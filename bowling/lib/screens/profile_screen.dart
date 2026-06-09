fbf6dc7c77f18e322c964cda61c8b9d521eb6210
rafli <oliviaoliv2006@gmail.com>
2026-06-09 11:54:59 +0700

    Update profile_screen.dart

--- bowling/lib/screens/profile_screen.dart
+++ bowling/lib/screens/profile_screen.dart
@@ -264,7 +264,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           ),
                         ),
                       ),
-                      ,
                     ],
                   ),
                 );
